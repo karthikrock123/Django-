fc1cf6800bbe4b51426b86a2503c2c9bd3faed83
karthikchoda <karthikhulkoo7@gmail.com>
2020-04-02 21:09:26 +0530

    second commit

--- GLUG/views.py
+++ GLUG/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Project,contact
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 def index(request):
     projects = Project.objects.all()
     return render(request,"index.html" , {'projects':projects})
+
+def login(request):
+    if request.method == 'POST':
+        username  = request.POST['username']
+        password  = request.POST['password']
+
+        user= auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/") 
+        else:
+            messages.info(request,'User Not Found')
+            return redirect("login")
+    else:
+        return render(request,"index.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
--- GLUGWebsite/urls.py
+++ GLUGWebsite/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('GLUG.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
