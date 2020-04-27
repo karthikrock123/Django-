de5e7c9cd3e9d47b179f78af2d0f3a30a94088a7
karthikchoda <karthikhulkoo7@gmail.com>
2020-04-27 13:35:14 +0530

    seventh commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
-def login(request):
+def login_request(request):
     if request.method == 'POST':
-        username  = request.POST['username']
-        password  = request.POST['password']
-
-        user= auth.authenticate(username=username,password=password)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            auth.login(request,user)
-            return redirect("/") 
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
+            return redirect("/")
         else:
-            messages.info(request,'User Not Found')
+            messages.error(request, "Invalid username or password.")
             return redirect("login")
     else:
-        return render(request,'login.html')
+        return render(request,"login.html")
 
-def logout(request):
+def logout_request(request):
     auth.logout(request)
     return redirect("/")
