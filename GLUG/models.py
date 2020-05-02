39162bbf82bd007f555313362c75aa6867edae18
karthikchoda <karthikhulkoo7@gmail.com>
2020-05-02 16:44:21 +0530

    Tenth commit

--- GLUG/models.py
+++ GLUG/models.py
@@ -4,7 +4,7 @@ class Project(models.Model):
     name        = models.CharField(max_length=100)
     description = models.TextField()
     icon        = models.TextField()
-    
+
     def __str__(self):
         return self.name
 
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
 from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.db.models.query import QuerySet
+from .models import listOfProject
+from GLUG.models import Project
 # Create your views here.
 
 def login_request(request):
@@ -24,4 +26,22 @@ def logout_request(request):
     return redirect("/")
 
 def project_request(request):
-    return render(request,"project_page.html")
+    pop=[];cs=[];ps=[]
+    if User.is_authenticated: 
+        a = request.user.id 
+        if(listOfProject.objects.filter(name_id=a)):
+            u = listOfProject.objects.filter(name = a)
+            us = u.values('projects')
+            #print(us)
+            for i in us:
+                pop.append(i)
+            #print(pop)
+            for i in range(0,len(pop)):
+                x = (pop[i].get('projects'))
+                cs.append(x)
+            #print(cs)
+            for i in cs:
+                p = Project.objects.filter(id = i)
+                ps.append(p.values('name'))
+            print(ps)
+        return render(request,"project_page.html",{'ps':ps})
